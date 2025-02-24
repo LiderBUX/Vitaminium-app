38129131205be8f2efeb4eec854dbc6caa8fb0de
LiderBUX <frcbybz97@mail.ru>
2025-02-24 10:07:53 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Vitaminium-app/', // Указываем путь для GitHub Pages
+  base: '/Vitaminium-app/', // Путь для GitHub Pages
 });
