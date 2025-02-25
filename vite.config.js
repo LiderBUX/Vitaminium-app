1092c0d9091e6a6b3c796efbdfb03a314607d69e
LiderBUX <frcbybz97@mail.ru>
2025-02-26 00:36:31 +0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Vitaminium-app/', // Путь для GitHub Pages
-});
